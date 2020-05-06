fa49d6be6738fa558581cbaa8ea73c3854126fcf
NN <NN@LAPTOP-A66RRBO9>
2020-05-06 19:02:43 +0200

    added functions

--- restaurant management/src/restaurant/Admin.java
+++ restaurant management/src/restaurant/Admin.java
@@ -1,5 +1,32 @@
 package restaurant;
 
 public class Admin {
+	//attributes
+	  public String username;
+	  private int password;
+	  //Employee e;
 
+	    public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+
+	public void alterPassword(int password) {
+		this.password=password;
+	}
+
+	public void alterUsername(String username) {
+		this.username=username;
+	}
 }
