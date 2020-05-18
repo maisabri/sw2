aba9a911f18c65ce20a909a153aef7a9656ac6cd
muhammed748 <62562910+muhammed748@users.noreply.github.com>
2020-05-18 07:15:58 +0200

    Update User.java
    added login

--- restaurant management/src/restaurant/User.java
+++ restaurant management/src/restaurant/User.java
@@ -2,4 +2,22 @@ package restaurant;
 
 public class User {
 
+    public String username ;
+    public String password ;
+
+    public User (){
+
+    }
+
+    public User( String str1 , String str2 )
+    {
+        this.username = str1 ;
+        this.password = str2 ;
+    }
+
+    public boolean login( String str1 , String str2 )
+    {
+         return str1.equals( this.username ) && str2.equals( this.password ) ;
+    }
+
 }
