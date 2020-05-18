858e30f0f44e0f92b559c2029bc9e714e7f04267
maisabri <62540503+maisabri@users.noreply.github.com>
2020-05-18 05:49:02 +0200

    Create Employee.java

---
+++ restaurant management/src/restaurant/Employee.java
@@ -0,0 +1,39 @@
+package restaurant;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+
+public class Employee {
+
+  EmployeeFile e;
+	Customer c;
+	Menu u;
+  private final ArrayList<Customer> customerList =new ArrayList<>();
+  public final ArrayList<File> file =new ArrayList<>();
+  static int i=0;
+  
+  void addCustomer(String Name_Customer) {
+            try {
+            file.add(new File("Customer"+ i + ".txt"));
+            Customer a=new Customer(Name_Customer);
+            Writer fileWriter= new FileWriter("Customer"+ i + ".txt");
+                
+            try (BufferedWriter pw = new BufferedWriter(fileWriter))
+            {
+                    customerList.add(a);
+                    pw.write("Customer's name : " + Name_Customer +"\t" + "ID : "+ i);
+                    i++;
+            }
+                }
+            catch(IOException e)
+            {
+                  System.err.println("error");
+            }
+         }
+         
+         
+}
