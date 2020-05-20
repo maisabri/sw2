85e50699acdd35272cb34178717487bd546aed00
yarasalama <62558995+yara797@users.noreply.github.com>
2020-05-20 04:41:06 +0200

    Update Item.java
    finished

--- restaurant management/src/restaurant/Item.java
+++ restaurant management/src/restaurant/Item.java
@@ -4,4 +4,16 @@ public class Item {
     String name;
     String category;
     double price;
+    
+     public Item(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return category+":\n"+name+"\t"+"$"+price;
+    }
+    
 }
