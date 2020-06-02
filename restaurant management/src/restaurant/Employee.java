19f65b04560fe53bcd2b64b976712d2e3ad30067
muhammed748 <62562910+muhammed748@users.noreply.github.com>
2020-06-02 17:44:21 +0200

    Update Employee.java
    finished update customer

--- restaurant management/src/restaurant/Employee.java
+++ restaurant management/src/restaurant/Employee.java
@@ -56,4 +56,18 @@ public class Employee {
            }
            return s;
        }
+	
+	void updateCustomer(int ID_Customer,String Name_Customer) {
+    	 try {
+            File a=new File("employee"+ ID_Customer + ".txt");
+    	    Writer fileWriter= new FileWriter(a);
+                 try (BufferedWriter pw = new BufferedWriter(fileWriter)) {
+                     pw.write("Customer's name : " + Name_Customer +"\r\n" + "Customer ID : "+ i + "\r\n");
+                     file.set(ID_Customer,a);
+                 }         
+   	    Customer b=new Customer(Name_Customer);
+   	    customerList.set(ID_Customer, b);
+    	 }
+
+     }
 }
