e7be3a6e4222c34a2954ab7b06781ce64402c921
yarasalama <62558995+yara797@users.noreply.github.com>
2020-06-02 17:32:18 -0700

    Create Interface1.java

---
+++ restaurant management/src/restaurant/Interface1.java
@@ -0,0 +1,20 @@
+package restauranttt;
+
+/**
+ *
+ * @author DELL LAPTOP
+ */
+public interface Interface1 {
+
+
+	interface forEmployee{
+		public void addOrder(Menu m);
+		 public void removeItem(Menu m);
+		 public Menu getItem(int index);
+		 public void clearOrder();
+		 public int getSize();
+     
+     
+     
+	}
+}
