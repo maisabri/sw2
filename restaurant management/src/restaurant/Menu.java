b435f62e2dacfba7210a8243b64de165a77a567c
nourattya-hub <62563661+nourattya-hub@users.noreply.github.com>
2020-06-06 17:26:22 +0200

    Update Menu.java

--- restaurant management/src/restaurant/Menu.java
+++ restaurant management/src/restaurant/Menu.java
@@ -115,5 +115,14 @@ public class Menu {
 			  }
 		  return 0;
    }
+	 public void updateFood(int i, String name1 , double price1) {
+	   Item a=new Item(name1,"food",price1);
+	   food.set(i-1, a);
+   }
+   
+   public void updateDrink(int i, String name1 , double price1) {
+	   Item a=new Item(name1,"drink",price1);
+	   food.set(i-1, a);
+   }
 	
 }
