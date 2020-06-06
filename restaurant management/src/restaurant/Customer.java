d6dbfb5fef4882ae6a17801143e5b19e9bb67e19
muhammed748 <62562910+muhammed748@users.noreply.github.com>
2020-06-06 22:40:47 +0200

    Create Customer.java

---
+++ restaurant management/src/restaurant/Customer.java
@@ -0,0 +1,18 @@
+public class Customer{
+     String name;
+    
+      int id=1;
+      public Customer(){
+      }
+      
+      public Customer(String name){
+          this.name=name;
+      }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
