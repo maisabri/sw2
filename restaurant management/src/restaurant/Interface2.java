9ecd53069e752653fb10eaf66168b7eb51762f55
moatazmohamedddd <65420519+moatazmohamedddd@users.noreply.github.com>
2020-06-08 01:51:34 +0200

    Create Interface2.java

---
+++ restaurant management/src/restaurant/Interface2.java
@@ -0,0 +1,11 @@
+public interface Interface2 {
+   
+	interface forAdmin{
+		 public String displayFood();
+		 public String displayDrinks();
+		 public int searchFood(String name);
+		 public int searchDrink(String name);
+		 public void addFood(String name,double price);
+		 public void addDrink(String name,double price);
+     
+       }
