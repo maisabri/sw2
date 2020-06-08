aecaffd17a86906769fa04c95683c3c406f7cd70
maisabri <62540503+maisabri@users.noreply.github.com>
2020-06-08 19:12:20 +0200

    finished

--- restaurant management/src/restaurant/Billing.java
+++ restaurant management/src/restaurant/Billing.java
@@ -1,10 +1,8 @@
+package restaurant;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-/**
- *
- * @author DELL LAPTOP
- */
+
 public class Billing {
      Scanner scan =new Scanner(System.in);
     DecimalFormat df = new DecimalFormat("0.0");
@@ -21,3 +19,53 @@ public class Billing {
     public void setMealCost(double mealCost) {
         this.mealCost = mealCost;
     }
+
+         public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getTip() {
+        return tip;
+    }
+
+    public void setTip(double tip) {
+        this.tip = tip;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public double getTotalBill() {
+        return totalBill;
+    }
+
+    public void setTotalBill(double totalBill) {
+        this.totalBill = totalBill;
+    }
+    
+     public double tax(){
+            tax = 0.067 * mealCost;
+            return tax;
+     }
+     
+      public double tip(){
+        totalCost = mealCost + tax;
+        tip= 0.2 * totalCost;
+            return tip;
+      }
+    
+       public double totalBill(){
+        totalBill = totalCost + tip;
+            return totalBill;
+    }
+     
+}
