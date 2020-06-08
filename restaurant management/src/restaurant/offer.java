305fd6b45b019e4b1696eae105cd76bef9e6d2c0
muhammed748 <62562910+muhammed748@users.noreply.github.com>
2020-06-08 18:47:26 +0200

    Update offer.java

--- restaurant management/src/restaurant/offer.java
+++ restaurant management/src/restaurant/offer.java
@@ -1,3 +1,5 @@
+package restaurant;
+import java.util.Scanner;
 public class Offer {
 
 
@@ -13,3 +15,16 @@ public class Offer {
         disc = read.nextDouble() ;
         read.close();
     }
+
+        public void newprice()
+    {
+    	Scanner read = new Scanner(System.in) ;
+        System.out.println("enter price before discount : ") ;
+        oldprice = read.nextDouble() ;
+        read.close();
+        newprice = oldprice - ( oldprice*disc/100 ) ;
+        System.out.println( "the new price is :" + newprice ) ;
+    }
+}
+
+
