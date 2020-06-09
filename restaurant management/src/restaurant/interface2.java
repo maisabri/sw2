804bd1925f389bb27e1b721ac263c1db883a724c
yarasalama <62558995+yara797@users.noreply.github.com>
2020-06-09 15:08:25 +0200

    done

--- restaurant management/src/restaurant/interface2.java
+++ restaurant management/src/restaurant/interface2.java
@@ -8,5 +8,9 @@ public interface Interface2 {
 		 public int searchDrink(String name);
 		 public void addFood(String name,double price);
 		 public void addDrink(String name,double price);
-     
+     		 public void removeFood (int i);
+		 public void removeDrink (int i);
+		 public void updateFood(int i,String name1,double price1);
+		 public void updateDrink(int i,String name1,double price1);
+	}
      }
